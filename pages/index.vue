4c13ad49f1bed9f463d8fef62022b798537d4f34
Mateus Ito Silva <mateusito10@gmail.com>
2021-09-15 15:33:38 -0300

    Home - add a simple home page to find apes

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,26 @@
 <template>
   <main class="main">
     <div class="main__container">
-      <div class="main__info">
-        <pre class="main__info--danger" v-if="error">Error</pre>
-        <pre v-else-if="loading">Loading...</pre>
-        <div v-else>
-          <span>
-            <h2 class="text-2xl">Ape:</h2>
-            <pre class="mt-2">{{ ape }}</pre>
-          </span>
-
-          <hr class="my-12" />
-
-          <span>
-            <h2 class="text-2xl">Worth:</h2>
-            <pre class="mt-2">{{ apeScore }}</pre>
-          </span>
+      <div v-if="ape">
+        <img :src="ape.image" alt="ape" />
+      </div>
+      <div>
+        <form @submit.prevent="filter" class="main__form">
+          <input type="number" class="main__form-input" v-model="apeId" />
+          <button class="main__form-submit">Filter</button>
+        </form>
+        <div class="main__info">
+          <pre>{{ ape }}</pre>
+          <hr class="my-5" />
+          <div v-if="error" class="main__info main__info--danger">Error.</div>
+          <div v-else-if="loading">Loading...</div>
+          <div v-else class="flex items-center justify-between">
+            <pre>{{ apeRarity }}</pre>
+            <div>
+              <span>Score</span>
+              <pre class="text-5xl">{{ apeScore }}</pre>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- pages/script.ts
+++ pages/script.ts
@@ -1,41 +1,32 @@
-import { defineComponent, ref, useFetch, Ref, reactive } from '@nuxtjs/composition-api'
+import { defineComponent, ref, watch } from '@nuxtjs/composition-api'
+import { getApe } from '~/api/apes'
 import { calculateApeRarity, calculateApeScorePoint } from '~/api/rarity'
-import { AllTraits } from '~/types'
 
 export default defineComponent({
   setup() {
-    const val = ref()
-    const loading = ref(true)
     const error = ref(false)
+    const loading = ref(false)
+    const ape = ref()
+    const apeId = ref()
+    const apeScore = ref()
+    const apeRarity = ref()
 
-    const collection = ref('bored')
-    const traitName: Ref<keyof AllTraits> = ref('Mouth')
-    const traitValue = ref('Bored')
-    const ape = reactive({
-      image: '',
-      imageHash: '',
-      tokenId: 0,
-      collection: 'bored',
-      traits: {
-        Background: 'Yellow',
-        Clothes: 'Striped Tee',
-        Eyes: 'Blindfold',
-        Mouth: 'Closed',
-        Fur: 'Red',
-        Hat: 'Yellow Horns',
-        Earring: 'Cross',
-      },
-    })
-    const apeScore = ref(0)
+    const filter = async () => {
+      loading.value = true
 
-    useFetch(async () => {
-      val.value = await calculateApeRarity(ape)
-      apeScore.value = await calculateApeScorePoint(ape)
+      ape.value = await getApe('bored', +apeId.value)
+      if (!ape.value) return (error.value = true)
 
-      if (val.value) loading.value = false
-      else error.value = true
-    })
+      apeRarity.value = await calculateApeRarity(ape.value)
+      if (!apeRarity.value) return (error.value = true)
 
-    return { val, ape, loading, error, collection, traitName, traitValue, apeScore }
+      apeScore.value = await calculateApeScorePoint(ape.value)
+      if (!apeScore.value) return (error.value = true)
+      else loading.value = false
+    }
+
+    watch(apeId, () => (error.value = false))
+
+    return { ape, apeId, filter, apeRarity, apeScore, loading, error }
   },
 })
