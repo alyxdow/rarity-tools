f149730ec643bcb14bc1b10d6bc1f2bf41a3b429
Mateus Ito Silva <mateusito10@gmail.com>
2021-09-18 09:42:32 -0300

    InfoToast - add component to show info

---
+++ components/atoms/InfoToast/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="toast">
+    <span class="toast__text">{{ text }}</span>
+    <div class="toast__info">{{ info }}</div>
+  </div>
+</template>
+
+<script lang="ts" src="./script.ts"></script>
+<style lang="scss" src="./style.scss"></style>
---
+++ components/atoms/InfoToast/script.ts
@@ -0,0 +1,8 @@
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    text: String,
+    info: String,
+  },
+})
