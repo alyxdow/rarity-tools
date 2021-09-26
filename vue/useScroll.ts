4a6cb933123a7174879fd4385906cb0969ebe1b1
Mateus Ito Silva <mateusito10@gmail.com>
2021-09-26 12:23:54 -0300

    useScroll - comments

--- vue/useScroll.ts
+++ vue/useScroll.ts
@@ -1,5 +1,11 @@
 import { onMounted, onUnmounted, ref } from '@nuxtjs/composition-api'
 
+/**
+ * Composable to listen to scroll event to window
+ * @param amount of pixels to Y offset
+ * @returns a boolean to verify the page scroll
+ */
+
 export const useScroll = (amount: number) => {
   const hasScrolled = ref(false)
 
