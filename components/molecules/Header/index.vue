d5312c67adffc469e809d86fe20df0810a122a72
Mateus Ito Silva <mateusito10@gmail.com>
2021-09-26 15:07:30 -0300

    Header - add header links

--- components/molecules/Header/index.vue
+++ components/molecules/Header/index.vue
@@ -1,23 +1,47 @@
 <template>
   <header class="header">
-    <div class="header__logo" @click="goHome">
-      <div class="header__logo--mobile">
-        <transition name="icon">
-          <img
-            src="~/assets/svg/logo-icon.svg"
-            alt="Logo"
-            v-if="mobileScroll"
-            class="header__logo-icon"
-            :class="{ disabled: $route.fullPath != '/home' }"
-          />
-        </transition>
-        <img src="~/assets/svg/logo.svg" class="header__logo-img" alt="Logo" />
+    <div class="header__container">
+      <div class="header__logo" @click="goHome">
+        <div class="header__logo--mobile">
+          <transition name="icon">
+            <img
+              src="~/assets/svg/logo-icon.svg"
+              alt="Logo"
+              v-if="mobileScroll"
+              class="header__logo-icon"
+              :class="{ disabled: $route.fullPath != '/home' }"
+            />
+          </transition>
+          <img src="~/assets/svg/logo.svg" class="header__logo-img" alt="Logo" />
+        </div>
+
+        <div class="header__logo--desktop">
+          <img src="~/assets/svg/logo-icon.svg" class="header__logo-icon" alt="Logo" :class="{ scrolled: desktopScroll }" />
+          <img src="~/assets/svg/logo-text.svg" class="header__logo-text" alt="Logo" />
+        </div>
       </div>
 
-      <div class="header__logo--desktop">
-        <img src="~/assets/svg/logo-icon.svg" class="header__logo-icon" alt="Logo" :class="{ scrolled: desktopScroll }" />
-        <img src="~/assets/svg/logo-text.svg" class="header__logo-text" alt="Logo" />
-      </div>
+      <nav class="header__nav">
+        <ul class="header__nav-list">
+          <li class="header__nav-item">
+            <a
+              href="https://odd-cave-7cc.notion.site/Apexgo-io-a3e697ef5cc14ad782112c52cb627896"
+              class="header__nav-link"
+              target="_blank"
+            >
+              Learn
+            </a>
+          </li>
+
+          <li class="header__nav-item">
+            <a href="#" class="header__nav-link">Get in Touch</a>
+          </li>
+
+          <li class="header__nav-item">
+            <a href="#" class="header__nav-link">Submit</a>
+          </li>
+        </ul>
+      </nav>
     </div>
 
     <form class="header__form" @submit.prevent="evaluateApe">
