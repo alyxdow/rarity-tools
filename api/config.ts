98c0a5356418be53b5205c9542decbfb32d88192
Mateus Ito Silva <mateusito10@gmail.com>
2021-09-30 01:08:28 -0300

    Nuxt, Config - fix url undefined

--- api/config.ts
+++ api/config.ts
@@ -5,7 +5,7 @@
  */
 
 export const dev = process.env.NODE_ENV !== 'production'
-export const url = dev ? 'http://localhost:3000' : process.env.SEO_PERMALINK
+export const url = dev ? 'http://localhost:3000' : process.env.baseUrl
 
 /**
  * |--------------------------------------------------------------------------------------------------------------------------|
