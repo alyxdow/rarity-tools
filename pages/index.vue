92837ad0666d99ae9c355d9ab093c04f13ddfdd8
Mateus Ito Silva <mateusito10@gmail.com>
2021-10-04 00:06:31 -0300

    Home - fix /script render

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,40 @@
+<script lang="ts">
+import { defineComponent, ref, onMounted } from '@nuxtjs/composition-api'
+import { collections, url } from '~/api/config'
+import useSetFavicon from '~/vue/useSetFavicon'
+
+export default defineComponent({
+  setup() {
+    // Apes ------------------------------------------------------------------------------------------------------------------|
+    const nfts = ref()
+    const getFirstApes = async () => {
+      nfts.value = collections.map(collection => ({
+        value: collection.value,
+        url: `${url}/collections/${collection.value}.json`,
+      }))
+    }
+
+    // Favicon ---------------------------------------------------------------------------------------------------------------|
+    useSetFavicon()
+
+    // Gallery views
+    const view = ref('gallery')
+
+    // On instance mounted ---------------------------------------------------------------------------------------------------|
+    onMounted(() => {
+      getFirstApes()
+    })
+
+    // Return values ---------------------------------------------------------------------------------------------------------|
+    return {
+      nfts,
+      view,
+      collections,
+    }
+  },
+})
+</script>
+
 <template>
   <div class="main">
     <Gallery 
@@ -11,5 +48,14 @@
   </div>
 </template>
 
-<script lang="ts" src="./script.ts"></script>
-<style lang="scss" src="./style.scss"></style>
+<style lang="scss">
+.ape-enter-from,
+.ape-leave-to {
+  opacity: 0
+}
+
+.ape-enter-active,
+.ape-leave-active {
+  transition: all 0.3s ease
+}
+</style>
